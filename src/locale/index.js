d2688246ab9cb0f16840000728d9b7bb4717c753
donnischen <82943858+donnischen@users.noreply.github.com>
2022-03-13 18:52:45 +0800

    Add files via upload

---
+++ src/locale/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueClipboard from 'vue-clipboard2';
+import VueI18n from 'vue-i18n';
+import store from '@/store';
+
+import en from './lang/en.js';
+import zhCN from './lang/zh-CN.js';
+import zhTW from './lang/zh-TW.js';
+
+Vue.use(VueClipboard);
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: store.state.settings.lang,
+  messages: {
+    en,
+    'zh-CN': zhCN,
+    'zh-TW': zhTW,
+  },
+  silentTranslationWarn: true,
+});
+
+export default i18n;
